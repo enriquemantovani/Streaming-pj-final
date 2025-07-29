a0bf5e2d48c41447b01a1dac42a1617c0a96bb67
Rodrigo <rodrigosvarginha@hotmail.com>
2025-07-29 16:15:37 -0300

    Codigo da pagina generos consertado, design refinado, e arquivos desnecessarios apagados

--- src/app/generos/page.tsx
+++ src/app/generos/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function GenerosPage(){
 
     return(
@@ -5,49 +7,53 @@ export default function GenerosPage(){
 
         <main className="flex-grow">
 
-            <div className="flex flex-col gap-20 bg-blue-500">
+            <div className="flex flex-col items-center gap-20 bg-blue-500">
 
-                <section className="bg-gradient-to-r from-blue-700 via-blue-900 to-blue-700 py-20">
-                    <div className="flex flex-col items-center gap-6 container mx-auto">
-                        <h1 className="text-4xl text-white font-bold">Procure pelos seus gêneros favoritos!</h1>
+                {/* Section da pagina generos */}
+
+                <section className="bg-gradient-to-r from-blue-700 via-blue-900 to-blue-700 py-20 w-full">
+                    <div className="flex flex-col items-center gap-6">
+                        <p className="text-4xl text-white font-bold">Procure pelos seus gêneros favoritos!</p>
                         <p className="text-xl text-white"> Escolha o filme que mais bem combina com você e aproveite! </p>
                     </div>
                 </section>
 
-                <div className="flex flex-col items-center container mx-auto gap-8 border-2 max-w-2/3 bg-blue-800 my-20">
+                {/* Menu da pagina generos */}
+
+                <div className="flex flex-col items-center gap-8 border-2 border-white max-w-2/3 min-w-2/3 bg-blue-800 my-20">
 
                     <div className="flex justify-center min-w-1/8 border-b-2 border-l-2 border-r-2 border-black text-2xl">
                         <h2>Menu</h2>
                     </div>
 
-                    <div className="flex w-full">
-                        <div className="bg-white w-3/5 py-28">
+                    <div className="flex w-full min-h-[300px]">
+                        <div className="bg-white w-3/5 border-2 border-black">
                             <p>Lorem</p>
                         </div>
 
-                        <div className="flex justify-center bg-gray-400 w-8 py-28">
+                        <div className="flex justify-center items-center bg-gray-400 border-t-2 border-r-2 border-b-2 border-black w-8">
                             <p className="text-3xl">‖</p>
                         </div>
 
                     </div>
 
-                    <div className="flex justify-around w-full border-2 my-8 border-white">
+                    <div className="flex justify-around w-full my-8">
                         <div className="flex gap-8">
-                            <button className="bg-green-400 text-white text-xl font-bold border border-black hover:bg-green-500 transition duration-400">
+                            <button className="bg-green-400 text-white text-xl font-bold border border-black rounded-xl hover:bg-green-600 transition duration-400">
                                 Criar gênero
                             </button>
 
-                            <button className="bg-green-400 text-white text-xl font-bold border border-black hover:bg-green-500 transition duration-400">
+                            <button className="bg-green-400 text-white text-xl font-bold border border-black rounded-xl hover:bg-green-600 transition duration-400">
                                 Listar Todos 
                             </button>
                         </div>
 
                         <div className="flex gap-8">
-                            <button className="bg-amber-400 text-white text-xl font-bold border border-black hover:bg-amber-500 transition duration-400 p-2 px-4">
+                            <button className="bg-amber-400 text-white text-xl font-bold border border-black rounded-xl hover:bg-amber-500 transition duration-400 py-2 px-4">
                                 Editar
                             </button>
 
-                            <button className="bg-red-600 text-white text-xl font-bold border border-black hover:bg-red-700 transition duration-400 p-2 px-4">
+                            <button className="bg-red-600 text-white text-xl font-bold border border-black rounded-xl hover:bg-red-800 transition duration-400 py-2 px-4">
                                 Deletar
                             </button>
                         </div>
